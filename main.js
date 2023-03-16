798a18e2a70fad630f35d16ea2203e8ca9245cff
rurbano-wisc <120360519+rurbano-wisc@users.noreply.github.com>
2023-03-15 20:19:21 -0700

    lesson 1 svg complete

--- main.js
+++ main.js
@@ -2,15 +2,26 @@
 //execute script when window is loaded
 window.onload = function(){
 
+    //SVG dimension variables
+     var w = 900, h = 500;
+    //container block
     var container = d3.select("body") //get the <body> element from the DOM
     .append("svg") //put a new svg in the body
     .attr("width", w) //assign the width
     .attr("height", h) //assign the height
     .attr("class", "container") //assign a class name
     .style("background-color", "rgba(0,0,0,0.2)")
+    .append("rect") //add a <rect> element
+        .attr("width", 800) //rectangle width
+        .attr("height", 400) //rectangle height
+
+    // <rect> is now the operand of the container block
      //innerRect block
      var innerRect = container.append("rect") //put a new rect in the svg
         .datum(400)
+        .attr("width", 800) //rectangle width
+        .attr("height", 400) //rectangle height
+
         .attr("width", function(d){ //rectangle width
             return d * 2; //400 * 2 = 800
         }) 
